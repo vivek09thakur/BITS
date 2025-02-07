9ea58ab8ebbdd068ecb777a46818b0f4ab803222
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-02-07 14:12:26 +0530

    commit

---
+++ SEM_1/CompProg/01-program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    char ch = "a";
+    int num = 1234;
+    float f_num = 12.56;
+    double d_num = 1234.0;
+
+    printf("character is %c \n",ch);
+    printf("Num is %d\n",num);
+    printf("float number is %f\n", f_num);
+    printf("double number is %lf\n", d_num);
+}
---
+++ SEM_1/CompProg/02-program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+    // char ch = "a";
+    // int num = 1234;
+    // float f_num = 12.56;
+    // double d_num = 1234.0;
+
+    // AFTER MODIFICATION
+    char ch = 97;
+    int num = 'a';
+    float f_num = 12;
+    double d_num = 1234;
+
+    printf("character is %c \n",ch);
+    printf("Num is %d\n",num);
+    printf("float number is %f\n", f_num);
+    printf("double number is %lf\n", d_num);
+}
