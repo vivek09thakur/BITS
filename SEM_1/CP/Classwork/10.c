9bc5ae5732a233512414302e03271a90c5917c7a
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-02-17 00:10:50 +0530

    commit

--- SEM_1/CP/Classwork/10.c
+++ SEM_1/CP/Classwork/10.c
@@ -1,1 +1,30 @@
 // TASK 10: 
+#include<stdio.h>
+
+// int main(){
+//     int x = 10;
+//     if (x > 5)
+//         if (x > 8)
+//             printf("A\n");
+//         else
+//         printf("B\n");
+//     return 0;
+    
+// }
+
+// void main(){
+//     int a =6,b = 23;
+//     if (a > b);
+//         printf("a is greater than b\n");
+//     else 
+//         printf("b is greater than a\n")
+// }
+
+int main(){
+    int x =7;
+    if (x%2)
+        printf("even\n");
+    else
+        printf("odd\n");
+    return 0;
+}
