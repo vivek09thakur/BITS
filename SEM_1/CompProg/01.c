a47e38f4dc078f91b98fe69f8f221b1b33080d85
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-04-07 20:33:56 +0530

    commit

---
+++ SEM_1/CompProg/01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d is: %d\n", num, factorial(num));
+    return 0;
+}
+
