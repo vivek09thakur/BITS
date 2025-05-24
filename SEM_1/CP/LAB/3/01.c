b7b4b791440807ab4373faaebc49a8bbcb19fc83
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-05-24 17:45:36 +0530

    commit

--- SEM_1/CP/LAB/3/01.c
+++ SEM_1/CP/LAB/3/01.c
@@ -2,13 +2,8 @@
 #include <stdio.h>
 
 int factorial(int n) {
-<<<<<<< HEAD
-    if (n == 1) {
-        return 0;
-=======
     if (n == 0 || n == 1) {  // fxing the condition
         return 1; // fixing the return value
->>>>>>> f28c3ed4cdff4d6c48b6bdb136ac475300efe6a2
     } else {
         return n * factorial(n - 1);
     }
